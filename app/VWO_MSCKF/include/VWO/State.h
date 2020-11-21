587deac1fdae88d288721d1e23171f6fff04401a
yds <ydsf16@163.com>
2020-11-21 20:48:17 +0800

    Remove delta x limitation

--- app/VWO_MSCKF/include/VWO/State.h
+++ app/VWO_MSCKF/include/VWO/State.h
@@ -25,36 +25,6 @@ struct State {
     Eigen::MatrixXd covariance;
 
     void Update(const Eigen::VectorXd& delta_x) {
-        constexpr double kMaxDeltaRot = 5. * kDeg2Rad; // rad.
-        constexpr double kMaxDeltaTrans = 0.5; // meter.
-
-        const Eigen::Vector3d delta_ang = delta_x.segment(wheel_pose.state_idx, 3);
-        if (delta_ang.norm() > kMaxDeltaRot) { 
-            LOG(ERROR) << "Delta rot of wheel pose is too large. " << std::fixed << delta_ang.norm();
-            return; 
-        }
-
-        const Eigen::Vector3d delta_pos = delta_x.segment(wheel_pose.state_idx + 3, 3);
-        if (delta_pos.norm() > kMaxDeltaTrans) { 
-            LOG(ERROR) << "Delta trans of wheel pose is too large. " << std::fixed << delta_pos.norm();
-            return; 
-
-        }
-
-        for (CameraFramePtr& cam_fm : camera_frames) {
-            const Eigen::Vector3d delta_ang = delta_x.segment(cam_fm->state_idx, 3);
-            if (delta_ang.norm() > kMaxDeltaRot) { 
-                LOG(ERROR) << "Delta rot of camera pose is too large. " << std::fixed << delta_ang.norm();
-                return; 
-            }
-
-            const Eigen::Vector3d delta_pos = delta_x.segment(cam_fm->state_idx + 3, 3);
-            if (delta_pos.norm() > kMaxDeltaTrans) { 
-                LOG(ERROR) << "Delta trans of camera pose is too large. " << std::fixed << delta_pos.norm();
-                return; 
-            }
-        }
-
         // Update Wheel_pose.
         wheel_pose.Update(delta_x.segment(wheel_pose.state_idx, wheel_pose.size));
 
