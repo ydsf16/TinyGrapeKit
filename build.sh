4137b4a38ce1bb0cf93155cdb0098c338ceecb95
yds <ydsf16@163.com>
2020-11-21 22:09:30 +0800

    Add build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+## Build third party.
+cd third_party
+cd Pangolin
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j8
+
+## Build TinyGrapeKit library.
+cd ../../../
+cd library
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make install -j8
+
+## Build VWO-MSCKF
+cd ../../
+cd app/VWO_MSCKF
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j8
