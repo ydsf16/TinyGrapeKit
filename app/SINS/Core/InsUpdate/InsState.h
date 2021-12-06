18c1340a0c3b8dc621d19c5d97c12a4d99570c4f
yds_f16 <ydsf16@163.com>
2021-12-06 19:57:50 +0800

    Add INS updater.

---
+++ app/SINS/Core/InsUpdate/InsState.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Eigen/Eigen>
+
+namespace SINS {
+
+struct InsState {
+    double time;  // [s]
+
+    // PVA.
+    Eigen::Vector3d lon_lat_hei;     // [rad, rad, m] 
+    Eigen::Vector3d velocity;        // v_nb
+    Eigen::Quaterniond orientation;  // q_nb
+
+    // IMU params.
+    Eigen::Vector3d gyro_bias;
+    Eigen::Vector3d acc_bias;
+
+    // Earth params.
+    bool update_earth = false;
+    Eigen::Vector3d gravity = Eigen::Vector3d(0.0, 0.0, -9.8);
+    double Rm;
+    double Rn;
+    Eigen::Vector3d wie; 
+    Eigen::Vector3d wen; 
+    Eigen::Vector3d win; 
+};
+
+}  // namespace SINS
--- app/SINS/Examples/StationaryCoarseAlignmentExample.cpp
+++ app/SINS/Examples/StationaryCoarseAlignmentExample.cpp
@@ -1,25 +1,21 @@
 #include "Alignment/StationaryCoarseAlignment.h"
+#include "Utils/SO3.h"
 
 #include <iostream>
 
-Eigen::Vector3d RotMatToEuler(const Eigen::Matrix3d &C) {
-    double roll = std::atan2(C(2, 1), C(2, 2));
-    double pitch = std::atan2(-C(2, 0), sqrt(C(2, 1)* C(2, 1) + C(2, 2) * C(2, 2)));
-    double yaw = std::atan2(C(1, 0), C(0, 0));
-    return Eigen::Vector3d(yaw, pitch, roll);
-}
+using namespace SINS;
 
 int main(int argc, char **argv) {
     // Start.
-    const Eigen::Vector3d gyro(4.85072550613717e-07, -2.57168325026221e-07, 4.78749072084102e-07);
-    const Eigen::Vector3d acc(-0.00220864776504132, -0.000983490241384058, 0.0980537484095091);
+    // const Eigen::Vector3d gyro(4.85072550613717e-07, -2.57168325026221e-07, 4.78749072084102e-07);
+    // const Eigen::Vector3d acc(-0.00220864776504132, -0.000983490241384058, 0.0980537484095091);
 
     // End.
-    // const Eigen::Vector3d gyro(-5.17745291091245e-07,	2.09718176638119e-07,	4.59047968234761e-07);
-     //const Eigen::Vector3d acc(-0.00230025125136539,	-7.95820184872582e-06,	0.0980495373619216);
+    const Eigen::Vector3d gyro(-5.17745291091245e-07,	2.09718176638119e-07,	4.59047968234761e-07);
+    const Eigen::Vector3d acc(-0.00230025125136539,	-7.95820184872582e-06,	0.0980495373619216);
 
     Eigen::Matrix3d C_nb = SINS::StationaryCoarseAlign(acc, gyro); // 
-    const Eigen::Vector3d euler_nb = RotMatToEuler(C_nb); 
+    const Eigen::Vector3d euler_nb = MatToAtt(C_nb); 
 
     constexpr double kRadToDeg = 180. / M_PI;    
     std::cout << "C_nb:\n" << std::fixed << C_nb << std::endl << std::endl << std::endl;
